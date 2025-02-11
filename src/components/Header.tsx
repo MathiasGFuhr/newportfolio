c41b1ba8ab36808356273b9bb32016fac963ccb5
MathiasGFuhr <mathiasgfuhr@gmail.com>
2025-02-11 16:18:22 -0300

    edit header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,7 +52,7 @@ const Header = () => {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            Seu Nome
+            Mathias Fuhr
           </motion.h1>
 
           {/* Desktop Navigation */}
